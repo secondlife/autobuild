29f79576184fe103f86c5f88fa9eae9dc1452158
gabriel lee <gabriel@lindenlab.com>
2010-04-23 13:58:52 +0100

    spaces replacing tabs

--- autobuild/autobuild_base.py
+++ autobuild/autobuild_base.py
@@ -27,30 +27,30 @@ class autobuild_base:
 
 # Override these three functions to hook into autobuild.py
 
-	def get_details(self):
-		# name is the tool name - ie 'example' (for use from autobuild.py)
-		# when run from autobuild 'description' forms the help for this subcommand
-		# when run standalone it forms the tool desciption
-		return dict(name='', description='')
-	
-	def register(self, parser):
-		pass
+    def get_details(self):
+        # name is the tool name - ie 'example' (for use from autobuild.py)
+        # when run from autobuild 'description' forms the help for this subcommand
+        # when run standalone it forms the tool desciption
+        return dict(name='', description='')
+    
+    def register(self, parser):
+        pass
 
-	def run(self, args):
-		pass
+    def run(self, args):
+        pass
 
 # Standalone functionality:
 
-	# not __init__ as we have to overload functions it calls
-	def __init__(self):
-		details = self.get_details()
-		self.parser = argparse.ArgumentParser(description=details['description'])
-		self.register(self.parser)
-			
-	def main(self, args_in):
-		args = self.parser.parse_args(args_in)
-		self.run(args)
-		pass
+    # not __init__ as we have to overload functions it calls
+    def __init__(self):
+        details = self.get_details()
+        self.parser = argparse.ArgumentParser(description=details['description'])
+        self.register(self.parser)
+            
+    def main(self, args_in):
+        args = self.parser.parse_args(args_in)
+        self.run(args)
+        pass
 
 #if __name__ == "__main__":
 #	sys.exit( autobuild_base_standalone().main( sys.argv[1:] ) )
