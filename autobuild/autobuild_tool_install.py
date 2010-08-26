41fa42e863e4aa636d69aeaad21e12a0a94071ff
Andrew A. de Laix <alain@lindenlab.com>
2010-08-26 10:53:19 -0700

    added --get-source option to autobuild install which will check out source for a package in a parallel directory.

--- autobuild/autobuild_tool_install.py
+++ autobuild/autobuild_tool_install.py
@@ -30,6 +30,7 @@ import common
 import configfile
 import autobuild_base
 from llbase import llsd
+import subprocess
 
 AutobuildError = common.AutobuildError
 
@@ -118,6 +119,11 @@ def add_arguments(parser):
         default=False,
         dest='export_manifest',
         help="Print the install manifest to stdout and exit.")
+    parser.add_argument(
+        '--get-source',
+        action='append',
+        dest='get_source',
+        help="Get the source for this package instead of prebuilt binary.")
 
 def print_list(label, array):
     """
@@ -157,7 +163,7 @@ def handle_query_args(options, config_file, installed_file):
 
     return False
 
-def get_packages_to_install(packages, config_file, installed_config, platform):
+def get_packages_to_install(packages, config_file, installed_config, platform, as_source=[]):
     """
     Given the (potentially empty) list of package archives on the command line, work out
     the set of packages that are out of date and require a new download/install. This will
@@ -179,14 +185,21 @@ def get_packages_to_install(packages, config_file, installed_config, platform):
         if not toinstall:
             raise AutobuildError('Unknown package: %s' % pname)
 
-        # check that we have a platform-specific or common url to use
-        if not toinstall.archives_url(platform):
-           raise AutobuildError("No url specified for this platform for %s" % pname)
+        if pname in as_source:
+            if not toinstall.source:
+                raise AutobuildError("No source url specified for %" % pname)
+            if not toinstall.sourcetype:
+                raise AutobuildError("No source repository type specified for %" % pname)
+        else:
+            # check that we have a platform-specific or common url to use
+            if not toinstall.archives_url(platform):
+               raise AutobuildError("No url specified for this platform for %s" % pname)
 
         # install this package if it is new or out of date
         if installed == None or \
            toinstall.archives_url(platform) != installed.archives_url(platform) or \
-           toinstall.archives_md5(platform) != installed.archives_md5(platform):
+           toinstall.archives_md5(platform) != installed.archives_md5(platform) or\
+           pname in as_source:
             to_install.append(pname)
 
     return to_install
@@ -222,11 +235,13 @@ def post_install_license_check(packages, config_file, install_dir):
         if not os.path.exists(os.path.join(install_dir, licensefile)):
             raise AutobuildError("Invalid or undefined licensefile for %s. (you can use --skip-license-check)" % pname)
 
-def do_install(packages, config_file, installed_file, platform, install_dir, dry_run):
+def do_install(packages, config_file, installed_file, platform, install_dir, dry_run, as_source=[]):
     """
-    Install the specified list of packages. This will download the
+    Install the specified list of packages. By default this will download the
     packages to the local cache, extract the contents of those
-    archives to the install dir, and update the installed_file config.
+    archives to the install dir, and update the installed_file config.  For packages
+    listed in the optional 'as_source' list, the source will be downloaded in place
+    of the prebuilt binary.
     """
 
     # check that the install dir exists...
@@ -235,41 +250,94 @@ def do_install(packages, config_file, installed_file, platform, install_dir, dry
 
     # Filter for files which we actually requested to install.
     for pname in packages:
-
-        # find the url/md5 for the platform, or fallback to 'common'
         package = config_file.package(pname)
-        url = package.archives_url(platform)
-        md5 = package.archives_md5(platform)
-        cachefile = common.get_package_in_cache(url)
+        if pname in as_source:
+            _install_source(pname, package.sourcetype,
+                package.source, installed_file, config_file, dry_run)
+        else:
+            _install_binary(package, platform, config_file, install_dir, installed_file, dry_run)
 
-        # download the package, if it's not already in our cache
-        if not common.is_package_in_cache(url):
+def _install_source(pname, repotype, url, installed_config, config_file, dry_run):
+    if dry_run:
+        print "Dry run mode: not installing source for %s" % pname
+        return
+    installed_package = installed_config.package(pname)
+    if installed_package:
+        if installed_package.source != url:
+            raise AutobuildError("Source repository %s does not match installed %s" %
+                (url, installed_package.source))
+        if installed_package.sourcetype != repotype:
+            raise AutobuildError("Source repository type %s does not match installed type %s" %
+                (repotype, installed_package.sourcetype))
+    else:
+        installed_package = configfile.PackageInfo()
+    
+    # By convention source is downloaded into the parent directory containing this project.
+    sourcepath = os.path.normpath(
+        os.path.join(os.path.dirname(config_file.filename), os.pardir, pname))
+    if repotype == 'svn':
+        if os.path.isdir(sourcepath):
+            if subprocess.call(['svn', 'update', sourcepath]) != 0:
+                raise AutobuildError("Error updating %s" % pname)
+        else:
+            if subprocess.call(['svn', 'checkout', url, sourcepath]) != 0:
+                raise AutobuildError("Error checking out %s" % pname)
+    elif repotype == 'hg':
+        if os.path.isdir(sourcepath):
+            cwd = os.getcwd()
+            os.chdir(sourcepath)
+            try:
+                if subprocess.call(['hg', 'pull']) != 0:
+                    raise AutobuildError("Error pulling %s" % pname)
+                if subprocess.call(['hg', 'update']) != 0:
+                    raise AutobuildError("Error updating %s" % pname)
+            finally:
+                os.chdir(cwd)
+        else:
+            if subprocess.call(['hg', 'clone', url, sourcepath]) != 0:
+                raise AutobuildError("Error cloning %s" % pname)
+    else:
+        raise AutobuildError("Unsupported repository type %s" % repotype)
+    
+    installed_package.source = url
+    installed_package.sourcetype = repotype
+    installed_config.set_package(pname, installed_package)
+    
+def _install_binary(package, platform, configfile, install_dir, installed_file, dry_run):
+    # find the url/md5 for the platform, or fallback to 'common'
+    url = package.archives_url(platform)
+    md5 = package.archives_md5(platform)
+    cachefile = common.get_package_in_cache(url)
 
-            # download the package to the cache
-            if not common.download_package(url):
-                raise AutobuildError("Failed to download %s" % url)
+    # download the package, if it's not already in our cache
+    if not common.is_package_in_cache(url):
 
-            # error out if MD5 doesn't matches
-            if not common.does_package_match_md5(url, md5):
-                common.remove_package(url)
-                raise AutobuildError("md5 mismatch for %s" % cachefile)
+        # download the package to the cache
+        if not common.download_package(url):
+            raise AutobuildError("Failed to download %s" % url)
 
-        # dry run mode = download but don't install packages
-        if dry_run:
-            print "Dry run mode: not installing %s" % pname
-            continue
+        # error out if MD5 doesn't matches
+        if not common.does_package_match_md5(url, md5):
+            common.remove_package(url)
+            raise AutobuildError("md5 mismatch for %s" % cachefile)
 
-        # extract the files from the package
-        files = common.extract_package(url, install_dir)
+    # dry run mode = download but don't install packages
+    if dry_run:
+        print "Dry run mode: not installing %s" % pname
+        return
 
-        # update the installed-packages.xml file
-        package = installed_file.package(pname)
-        if not package:
-            package = configfile.PackageInfo()
-        package.set_archives_url(platform, url)
-        package.set_archives_md5(platform, md5)
-        package.set_archives_files(platform, files)
-        installed_file.set_package(pname, package)
+    # extract the files from the package
+    files = common.extract_package(url, install_dir)
+
+    # update the installed-packages.xml file
+    pname = package.name
+    package = installed_file.package(pname)
+    if not package:
+        package = configfile.PackageInfo()
+    package.set_archives_url(platform, url)
+    package.set_archives_md5(platform, md5)
+    package.set_archives_files(platform, files)
+    installed_file.set_package(pname, package)
 
 def install_packages(options, args):
     # write packages into 'packages' subdir of build directory by default
@@ -295,7 +363,10 @@ def install_packages(options, args):
         return 0
 
     # get the list of packages to actually installed
-    packages = get_packages_to_install(args, config_file, installed_file, options.platform)
+    if not options.get_source:
+        options.get_source = []
+    packages = get_packages_to_install(args, config_file, installed_file, 
+        options.platform, as_source=options.get_source)
 
     # handle any arguments to query for information
     if handle_query_args(options, config_file, installed_file):
@@ -307,7 +378,7 @@ def install_packages(options, args):
 
     # do the actual install of the new/updated packages
     do_install(packages, config_file, installed_file, options.platform, install_dir,
-               options.dry_run)
+               options.dry_run, as_source=options.get_source)
 
     # check the licensefile properties for the newly installed packages
     if options.check_license and not options.dry_run:
