be13f23b97dc327e6855372f77bc46cc0094dab7
Andrew A. de Laix <alain@lindenlab.com>
2010-09-29 15:59:19 -0700

    created Executable for encapsulating shell commands.

---
+++ autobuild/tests/test_executable.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import unittest
+from autobuild.executable import Executable
+
+class TestExecutable(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_simple_executable(self):
+        sleepExecutable = Executable(command='sleep', arguments=['1'])
+        result = sleepExecutable()
+        assert result == 0
+        
+    def test_compound_executable(self):
+        parentExecutable = Executable(command='grep', arguments=['foobarbaz','.'], options=['-l', '-r'])
+        childExecutable = Executable(parent=parentExecutable, options=['-i'])
+        otherChildExecutable = Executable(parent=parentExecutable, command='egrep', arguments=['foo','.'])
+        assert childExecutable.get_options() == ['-l', '-r', '-i']
+        assert childExecutable.get_command() == 'grep'
+        assert otherChildExecutable.get_command() == 'egrep'
+        assert otherChildExecutable.get_arguments() == ['foo','.']
+        result = childExecutable()
+        assert result == 0
+        result = parentExecutable()
+        assert result == 0
+ 
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
