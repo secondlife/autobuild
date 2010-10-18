21523a206b8d7703665811f6fb57daa69142d98d
jenn <jenn@lindenlab.com>
2010-10-18 09:13:34 -0700

    Completed first real test for autobuild_tool_edit.py. Constructs build config, compares, and passes.

--- autobuild/tests/test_edit.py
+++ autobuild/tests/test_edit.py
@@ -9,29 +9,25 @@ import subprocess
 import sys
 import unittest
 
+from llbase import llsd
 from autobuild import configfile
 from autobuild import common
 from autobuild.autobuild_main import Autobuild
 from baseline_compare import AutobuildBaselineCompare
-from autobuild.autobuild_tool_edit import AutobuildTool
+from autobuild.autobuild_tool_edit import AutobuildToolEdit
 
 
 class TestEdit(unittest.TestCase, AutobuildBaselineCompare):
     def setUp(self):
         os.environ["PATH"] = os.pathsep.join([os.environ["PATH"], os.path.abspath(os.path.dirname(__file__))])
         self.tmp_file = self.get_tmp_file(4)
-        self.config = configfile.ConfigurationDescription(self.tmp_file)
-        self.autobuild_tool = AutobuildTool()
+        self.edit_cmd = AutobuildToolEdit()
 
-    def _test_build(self):
-        expected_config = {'package_description': {'platforms': {'windows': {'configurations': {'newbuild': {'build': {'command': 'make this'}}}}}},
-                            'type': 'autobuild',
-                            'version': '1.2'}
-        args = ['build', 'name=newbuild', 'platform=windows', 'cmd="make this"']
-        self.autobuild_tool.main(args)
-        fh = open(self.tmp_file)
-        built_config = fh.read()
-        cmp_config = dict(built_config)
+    def test_build(self):
+        expected_config = {'package_description': {'platforms': {'windows': {'configurations': {'newbuild': {'build': {'command': 'make this', 'arguments': [], 'options': []}}}}}}, 'version': '1.2', 'type': 'autobuild'}
+        args = ['build', 'name=newbuild', 'platform=windows', 'cmd=make this', "--config-file=%s" % self.tmp_file]
+        self.edit_cmd.main(args)
+        built_config = llsd.parse(file(self.tmp_file, 'rb').read())
         assert (expected_config == built_config)
            
     def tearDown(self):
