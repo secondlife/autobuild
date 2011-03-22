2000382d44bd65d3c977b1914d142867cf9f6448
Nat Goodspeed <nat@lindenlab.com>
2011-03-22 09:44:17 -0400

    0.8.2: Update 'Home Page' URL from repo to Wiki page

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ from distutils.core import setup
 # Version twiddling
 # Each time we rebuild an autobuild package, manually increment the "build
 # number" here, e.g. 0.8.1, 0.8.2, etc.
-BUILD = 1
+BUILD = 2
 # But suppose we update our repository with new source and the version number
 # embedded in the package itself changes, e.g. from 0.8 to 0.9 -- but we don't
 # notice, simply incrementing the build number? The package build we expected
@@ -61,7 +61,7 @@ setup(
     version="%s.%s" % (AUTOBUILD_VERSION_STRING, BUILD),
     author='Brad Linden',
     author_email='brad@lindenlab.com',
-    url='http://hg.secondlife.com/autobuild/',
+    url="http://wiki.secondlife.com/wiki/Autobuild",
     description='Linden Lab Automated Package Management and Build System',
     platforms=["any"],
     package_dir={PACKAGE_NAME:LLAUTOBUILD_SOURCE},
