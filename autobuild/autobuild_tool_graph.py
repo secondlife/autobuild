325305c3dc241c9515495bfad9e54107d95c599a
Cinder <cinder.roxley@phoenixviewer.com>
2014-07-20 20:53:41 -0600

    PEP8 cleanup in autobuild_tool_graph.py

--- autobuild/autobuild_tool_graph.py
+++ autobuild/autobuild_tool_graph.py
@@ -33,7 +33,6 @@ Date   : 2014-05-09
 
 import os
 import sys
-import pprint
 import tempfile
 
 import pydot
@@ -46,6 +45,7 @@ import autobuild_base
 from autobuild_tool_install import extract_metadata_from_package
 
 logger = logging.getLogger('autobuild.graph')
+
 
 class GraphError(common.AutobuildError):
     pass
@@ -63,6 +63,7 @@ You may either:
 The --rebuild-from <package-name> option prints an ordered list of packages that
 must be rebuilt if the specified package is updated.
 """
+
 
 # define the entry point to this autobuild tool
 class AutobuildTool(autobuild_base.AutobuildBase):
@@ -89,9 +90,9 @@ class AutobuildTool(autobuild_base.AutobuildBase):
                             dest='platform',
                             default=common.get_current_platform(),
                             help='override the working platform')
-        parser.add_argument('-t','--type', 
+        parser.add_argument('-t', '--type',
                             dest='graph_type',
-                            choices=["dot","circo","neato","twopi","fdp","sfdp"],
+                            choices=["dot", "circo", "neato", "twopi", "fdp", "sfdp"],
                             default='dot',
                             help='which graphviz tool should be used to draw the graph')
         parser.add_argument('--install-dir',
@@ -117,14 +118,14 @@ class AutobuildTool(autobuild_base.AutobuildBase):
                 logger.warning("No complete metadata file found; attempting to use partial data from installed files")
                 # get the absolute path to the installed-packages.xml file
                 args.all = False
-                args.configurations=(args.configuration)
+                args.configurations = args.configuration
                 install_dirs = common.select_directories(args, config, "install", "getting installed packages",
                                                          lambda cnf:
                                                          os.path.join(config.get_build_directory(cnf, args.platform), "packages"))
                 installed_pathname = os.path.join(os.path.realpath(install_dirs[0]), args.installed_filename)
                 if os.path.exists(installed_pathname):
                     # dummy up a metadata object, but don't create the file
-                    metadata=configfile.MetadataDescription()
+                    metadata = configfile.MetadataDescription()
                     # use the package description from the configuration
                     metadata.package_description = config.package_description
                     metadata.add_dependencies(installed_pathname)
@@ -142,56 +143,55 @@ class AutobuildTool(autobuild_base.AutobuildBase):
         else:
             # assume that the file is a package archive and try to get metadata from it
             logger.info("searching for metadata in autobuild package file %s" % args.source_file)
-            metadata_stream=extract_metadata_from_package(args.source_file, configfile.PACKAGE_METADATA_FILE)
+            metadata_stream = extract_metadata_from_package(args.source_file, configfile.PACKAGE_METADATA_FILE)
             if metadata_stream is not None:
                 metadata = configfile.MetadataDescription(stream=metadata_stream)
                 if not metadata:
                     raise GraphError("No metadata found in archive '%s'" % args.file)
             
         if metadata:
-            graph=pydot.Dot(label=metadata['package_description']['name']+incomplete+' dependencies', graph_type='digraph')
-            graph.set('overlap','false')
-            graph.set('splines','true')
-            graph.set('scale','2')
-            graph.set('smoothType','spring')
-            graph.set('labelloc','top')
-            graph.set('labeljust','center')
+            graph = pydot.Dot(label=metadata['package_description']['name']+incomplete+' dependencies', graph_type='digraph')
+            graph.set('overlap', 'false')
+            graph.set('splines', 'true')
+            graph.set('scale', '2')
+            graph.set('smoothType', 'spring')
+            graph.set('labelloc', 'top')
+            graph.set('labeljust', 'center')
 
             graph.set_node_defaults(shape='box')
 
-            seen=dict()
             def add_depends(graph, pkg):
-                name=pkg['package_description']['name']
+                name = pkg['package_description']['name']
                 try:
                     pkg_node = graph.get_node(name)[0]
                 except IndexError:
                     pkg_node = None
                 if pkg_node is None:
                     logger.debug(" graph adding package %s" % name)
-                    pkg_node=pydot.Node(name)
-                    if 'dirty' in pkg and ( pkg['dirty'] == 'True' or pkg['dirty'] == True ):
+                    pkg_node = pydot.Node(name)
+                    if 'dirty' in pkg and (pkg['dirty'] == 'True' or pkg['dirty'] is True):
                         logger.debug(" setting %s dirty: %s" % (name, ("missing" if 'dirty' not in pkg else "explicit")))
                         pkg_node.set_shape('ellipse')
                         pkg_node.set_style('dashed')
                     graph.add_node(pkg_node)
                     if 'dependencies' in pkg:
                         for dep_pkg in pkg['dependencies'].itervalues():
-                            dep_name=dep_pkg['package_description']['name']
-                            dep_node=add_depends(graph, dep_pkg)
+                            dep_name = dep_pkg['package_description']['name']
+                            dep_node = add_depends(graph, dep_pkg)
                             logger.debug(" graph adding dependency %s -> %s" % (dep_name, name))
-                            edge=pydot.Edge(dep_name, name)
-                            if 'dirty' not in dep_pkg or dep_pkg['dirty'] == 'True' or dep_pkg['dirty'] == True:
+                            edge = pydot.Edge(dep_name, name)
+                            if 'dirty' not in dep_pkg or dep_pkg['dirty'] == 'True' or dep_pkg['dirty'] is True:
                                 edge.set_style('dashed')
                             graph.add_edge(edge)
                 return pkg_node
 
-            root=add_depends(graph, metadata)
+            root = add_depends(graph, metadata)
             root.set_root('true')
             root.set_shape('octagon')
             
             logger.debug("dot:\n"+graph.to_string())
 
-            graph_file=os.path.join(tempfile.gettempdir(),metadata['package_description']['name'] + "_graph_" + args.graph_type + '.png')
+            graph_file = os.path.join(tempfile.gettempdir(), metadata['package_description']['name'] + "_graph_" + args.graph_type + '.png')
 
             logger.info("writing %s" % graph_file)
             graph.write_png(graph_file, prog=args.graph_type)
