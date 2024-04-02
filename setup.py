17f3355536816948310e5b3aa5945301c488ffc7
Bennett Goble <signal@lindenlab.com>
2024-04-01 23:39:48 -0700

    Require llsd>=1.2.4, indent fix
    Fix autobuild.xml churn by requiring llsd 1.2.4.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=['llsd', 'pydot', 'pyzstd'],
+    install_requires=['llsd>=1.2.4', 'pydot', 'pyzstd'],
     extras_require={
         'dev': ['pytest', 'pytest-cov'],
         'build': ['build', 'setuptools_scm'],
