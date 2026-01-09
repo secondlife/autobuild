eb88bf69c5030e531053a3c0d6bc91695000e389
Andrey Kleshchev <117672381+akleshchev@users.noreply.github.com>
2026-01-10 00:38:08 +0200

    Lock pyzstd to a specific version to resolve issues with packaging

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    install_requires=['llsd>=1.2.4', 'pydot', 'pyzstd'],
+    install_requires=['llsd>=1.2.4', 'pydot', 'pyzstd==0.18.0'],
     extras_require={
         'dev': ['pytest', 'pytest-cov'],
         'build': ['build', 'setuptools_scm'],
